fcdd405c644d53a3f10c36a03d30a6e17ebbbd19
Alexey Makhmutov <makhmutov@netcracker.com>
2015-04-28 15:38:26 +0300

    Fix entity beans pool instances leakage on exceptions

--- ejb3/src/main/java/org/jboss/as/ejb3/component/entity/entitycache/TransactionLocalEntityCache.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/component/entity/entitycache/TransactionLocalEntityCache.java
@@ -95,7 +95,9 @@ public class TransactionLocalEntityCache implements ReadyEntityCache {
             final Object key = transactionSynchronizationRegistry.getTransactionKey();
             final Map<Object, CacheEntry> map = cache.get(key);
             if (map != null) {
-                map.remove(instance.getPrimaryKey());
+                if (map.remove(instance.getPrimaryKey())!=null) {
+                    component.discardEntityBeanInstance(instance);
+                }
             }
         }
     }
